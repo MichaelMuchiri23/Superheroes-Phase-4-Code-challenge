efeb02f97ac56903c7b8fa207ca5d81b9123bde4
Michael Muchiri <muchirimichael2222@gmail.com>
2024-01-29 23:48:20 +0300

    Redid the work

--- python-code-challenge-superheroes/code-challenge/app/seed.py
+++ python-code-challenge-superheroes/code-challenge/app/seed.py
@@ -1,5 +1,9 @@
 from app import app, db
-from models import Hero
+from datetime import datetime
+from models import Hero, Power, HeroPower
+from faker import Faker
+
+fake = Faker()
 
 all_heroes = [
   { "name": "Kamala Khan", "super_name": "Ms. Marvel" },
@@ -24,4 +28,41 @@ def seed_heroes():
             db.session.add(new_hero)
         db.session.commit()    
 
-seed_heroes()        
+def seed_powers():
+    powers_data = [
+        {"name": fake.word(), "description": fake.text()[:20]} for _ in range(10)
+    ]
+
+    for power_info in powers_data:
+        power = Power(**power_info, created_at=datetime.now(), updated_at=datetime.now())
+        db.session.add(power)
+
+def seed_hero_powers():
+    strengths = ["Strong", "Weak", "Average"]
+
+    heroes = Hero.query.all()
+
+    for hero in heroes:
+        for _ in range(1, 4):
+            power = Power.query.order_by(db.func.random()).first()
+            hero_power = HeroPower(
+                hero_id=hero.id,
+                power_id=power.id,
+                strength=fake.random_element(elements=strengths),
+                created_at=datetime.now(),
+                updated_at=datetime.now(),
+            )
+            db.session.add(hero_power)
+            db.session.commit()
+
+def seed_data():
+    seed_powers()
+    seed_heroes()
+    seed_hero_powers()
+
+if __name__ == '__main__':
+    with app.app_context():
+        print("Seeding started -----")
+        seed_data()
+        print("Seeded successfully")
+      
